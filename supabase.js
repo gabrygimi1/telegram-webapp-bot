63839b1d328c782dba9507a67565a79c0f7dc228
gabrygimi1 <93601054+gabrygimi1@users.noreply.github.com>
2025-11-13 06:20:57 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,17 @@
+// supabase.js
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_SERVICE_ROLE = process.env.SUPABASE_SERVICE_ROLE;
+
+if (!SUPABASE_URL) {
+  console.error("❌ SUPABASE_URL mancante nelle env di Render");
+  process.exit(1);
+}
+
+if (!SUPABASE_SERVICE_ROLE) {
+  console.error("❌ SUPABASE_SERVICE_ROLE mancante nelle env di Render");
+  process.exit(1);
+}
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE);
